d132180ce04abe252d9c8eee68727c176819da01
mesegur1 <116450465+mesegur1@users.noreply.github.com>
2023-06-28 22:17:20 -0700

    Flesh out single PID data load with Pandas

--- data_reader.py
+++ data_reader.py
@@ -2,6 +2,7 @@ import sys
 import struct
 import numpy as np
 import sklearn
+import pandas as pd
 import torch
 from tqdm import tqdm
 from sklearn.model_selection import train_test_split
@@ -41,19 +42,20 @@ def tac_to_class(tac: float):
         return TAC_LEVEL_1
 
 
-accel_data_full = []
+accel_data_full = pd.DataFrame([])  # = []
 
 
 # Load in accelerometer data into memory
 def load_accel_data_full():
     global accel_data_full
     print("Read in accelerometer data")
-    with open("data/all_accelerometer_data_pids_13.csv", "r", newline="") as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in tqdm(reader):
-            accel_data_full.append(row)
-        file.close()
+    accel_data_full = pd.read_csv("data/all_accelerometer_data_pids_13.csv")
+    accel_data_full["time"] = accel_data_full["time"].astype("datetime64[ms]")
+    accel_data_full["pid"] = accel_data_full["pid"].astype(str)
+    accel_data_full["x"] = accel_data_full["x"].astype(float)
+    accel_data_full["y"] = accel_data_full["y"].astype(float)
+    accel_data_full["z"] = accel_data_full["z"].astype(float)
+    accel_data_full = accel_data_full.set_index("time")
 
 
 # Load entire dataset
@@ -65,43 +67,44 @@ def load_data_combined(
 ):
     global accel_data_full
 
-    accel_data = [
-        (int(v[0]), str(v[1]), float(v[2]), float(v[3]), float(v[4]))
-        for v in accel_data_full
-    ]
-    # Skip first two rows
-    accel_data = accel_data[2:]
+    accel_data = accel_data_full
+    # skip two rows (dummy data)
+    accel_data = accel_data.iloc[2:-1]
+
+    # Down sample accelerometer data
+    accel_data = accel_data.resample("%dL" % (MS_PER_SEC / sample_rate)).first()
 
     tac_data = {}
     for pid in PIDS:
         print("Reading in Data for person %s" % (pid))
-        tac_data[pid] = []
-        with open("data/clean_tac/" + pid + "_clean_TAC.csv", "r", newline="") as file:
-            reader = csv.reader(file)
-            next(reader)
-            for row in reader:
-                tac_data[pid].append(row)
-            file.close()
-        tac_data[pid] = [
-            (int(v[0]) * 1000, tac_to_class(float(v[1]))) for v in tac_data[pid]
-        ]
-    # Get TAC labels for each accel data sample
-    tac_data_labels = []
-    for a in accel_data:
-        for d in tac_data[a[1]]:
-            if d[0] > a[0]:
-                tac_data_labels.append(d[1])
-                break
-    accel_data = accel_data[0 : len(tac_data_labels) : int(sample_rate / 10)]
-    tac_data_labels = tac_data_labels[0 : len(tac_data_labels) : int(sample_rate / 10)]
-    # Remove PID
-    accel_data = [(v[0], v[2], v[3], v[4]) for v in accel_data]
+        tac_data[pid] = pd.read_csv("data/clean_tac/%s_clean_TAC.csv" % pid)
+        tac_data[pid]["timestamp"] = tac_data[pid]["timestamp"].astype("datetime64[s]")
+        tac_data[pid]["TAC_Reading"] = tac_data[pid]["TAC_Reading"].astype(float)
+        # Get formatted TAC data
+        tac_data[pid]["TAC_Reading"] = (
+            tac_data[pid]["TAC_Reading"].map(lambda tac: tac_to_class(tac)).astype(int)
+        )
+        tac_data[pid] = tac_data[pid].rename(columns={"timestamp": "time"})
+        tac_data[pid] = tac_data[pid].set_index("time")
+
+    # Combine Data Frames to perform interpolation and backfilling
+    input_data = accel_data.join(list(tac_data.values()), how="outer")
+    input_data = input_data.apply(pd.Series.interpolate, args=("time",))
+    input_data = input_data.fillna(method="backfill")
+    input_data["time"] = input_data.index
+    input_data["time"] = input_data["time"].astype("int64")
+
+    print("Total Data length: %d" % (len(input_data.index)))
+
+    # Split data back into two parts for train/test set creation
+    accel_data = input_data[["time", "x", "y", "z"]].to_numpy()
+    tac_data_labels = input_data["TAC_Reading"].to_numpy()
 
     print("Creating data sets")
     # Split data into two parts
     train_data_accel, test_data_accel, train_data_tac, test_data_tac = train_test_split(
-        np.array(accel_data),
-        np.array(tac_data_labels),
+        accel_data,
+        tac_data_labels,
         test_size=test_ratio,
         shuffle=False,
     )
@@ -148,76 +151,49 @@ def load_data(
 ):
     global accel_data_full
     print("Reading in Data for person %s" % (pid))
-    tac_data = []
-    with open("data/clean_tac/%s_clean_TAC.csv" % pid, "r", newline="") as file:
-        reader = csv.reader(file)
-        next(reader)
-        for row in reader:
-            tac_data.append(row)
-        file.close()
+    tac_data = pd.read_csv("data/clean_tac/%s_clean_TAC.csv" % pid)
+    tac_data["timestamp"] = tac_data["timestamp"].astype("datetime64[s]")
+    tac_data["TAC_Reading"] = tac_data["TAC_Reading"].astype(float)
+    # Get formatted TAC data
+    tac_data["TAC_Reading"] = (
+        tac_data["TAC_Reading"].map(lambda tac: tac_to_class(tac)).astype(int)
+    )
+    tac_data = tac_data.rename(columns={"timestamp": "time"})
+    tac_data = tac_data.set_index("time")
 
     # Get specific accelerometer data
-    accel_data_specific = [
-        (int(v[0]), float(v[2]), float(v[3]), float(v[4]))
-        for v in accel_data_full
-        if v[1] == pid
-    ]
-    if limit > len(accel_data_specific):
-        limit = len(accel_data_specific)
-    accel_data_specific = accel_data_specific[offset:limit]
+    accel_data_specific = accel_data_full.query("pid == @pid")
     if pid == "JB3156" or pid == "CC6740":
         # skip first row (dummy data)
-        accel_data_specific = accel_data_specific[1:-1]
+        accel_data_specific = accel_data_specific.iloc[1:-1]
 
     # Down sample accelerometer data
-    accel_data = []
-    sample_data = []
-    current_time = accel_data_specific[0][0]
-    for v in accel_data_specific:
-        if v[0] < current_time + MS_PER_SEC:
-            sample_data.append(v)
-        else:
-            sample_data_size = len(sample_data)
-            if sample_data_size > sample_rate:
-                step = int(sample_data_size / sample_rate)
-                for i in range(0, sample_data_size, step):
-                    accel_data.append(sample_data[i])
-            else:
-                accel_data.extend(sample_data)
-            current_time += MS_PER_SEC
-            sample_data = []
-    accel_data_length = len(accel_data)
+    accel_data = accel_data_specific.resample(
+        "%dL" % (MS_PER_SEC / sample_rate)
+    ).first()
 
-    # Get formatted TAC data
-    tac_data = [(int(v[0]) * 1000, tac_to_class(float(v[1]))) for v in tac_data]
-    tac_data_length = len(tac_data)
-    tac_data_labels = []
-    i = 0
-    j = 0
-    if accel_data[0][0] > tac_data[0][0]:
-        # Cut off data before start of Accel data
-        while j < tac_data_length and accel_data[0][0] > tac_data[j][0]:
-            j += 1
-    elif accel_data[0][0] < tac_data[0][0]:
-        # Cut off data before start of TAC data
-        while i < accel_data_length and accel_data[i][0] < tac_data[0][0]:
-            i += 1
-    tac_data_labels.append(tac_data[j][1])
-    acc_new_start = i
-    while i < accel_data_length and j < tac_data_length:
-        if accel_data[i][0] < tac_data[j][0]:
-            tac_data_labels.append(tac_data[j][1])
-        else:
-            j = j + 1  # Move to next TAC entry
-        i = i + 1  # Go to next accel data
-    accel_data = accel_data[acc_new_start : acc_new_start + len(tac_data_labels)]
-    print("Total Data length: %d" % (len(tac_data_labels)))
+    # if limit > len(accel_data_specific.index):
+    #     limit = len(accel_data_specific.index)
+    # accel_data_specific = accel_data_specific.iloc[offset:limit]
+
+    # Combine Data Frames to perform interpolation and backfilling
+    input_data = accel_data.join(tac_data, how="outer")
+    input_data = input_data.apply(pd.Series.interpolate, args=("time",))
+    input_data = input_data.fillna(method="backfill")
+    input_data["time"] = input_data.index
+    input_data["time"] = input_data["time"].astype("int64")
+
+    print("Total Data length: %d" % (len(input_data.index)))
+
+    # Split data back into two parts for train/test set creation
+    accel_data = input_data[["time", "x", "y", "z"]].to_numpy()
+    tac_data_labels = input_data["TAC_Reading"].to_numpy()
 
     print("Creating data sets")
     # Split data into two parts
     train_data_accel, test_data_accel, train_data_tac, test_data_tac = train_test_split(
-        np.array(accel_data),
-        np.array(tac_data_labels),
+        accel_data,
+        tac_data_labels,
         test_size=test_ratio,
         shuffle=False,
     )
