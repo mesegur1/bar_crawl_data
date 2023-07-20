d68191c58beeba725ee030f2139ecd57dd5d9fb8
Manuel Segura <Manuel.E.Segura@jpl.nasa.gov>
2023-07-20 08:18:21 -0700

    Add frequency features to RBF encoder

--- encoders/HdcRbfEncoder.py
+++ encoders/HdcRbfEncoder.py
@@ -16,17 +16,23 @@ class HdcRbfEncoder(torch.nn.Module):
         # RBF Kernel Trick
         if use_tanh == True:
             self.kernel1 = embeddings.HyperTangent(
-                timestamps * 3, out_dimension, dtype=torch.complex64
+                timestamps * 3, out_dimension, dtype=torch.float64
             )
             self.kernel2 = embeddings.HyperTangent(
-                timestamps * 3, out_dimension, dtype=torch.complex64
+                timestamps * 3, out_dimension, dtype=torch.float64
+            )
+            self.kernel3 = embeddings.HyperTangent(
+                timestamps * 3, out_dimension, dtype=torch.float64
             )
         else:
             self.kernel1 = embeddings.Sinusoid(
-                timestamps * 3, out_dimension, dtype=torch.complex64
+                timestamps * 3, out_dimension, dtype=torch.float64
             )
             self.kernel2 = embeddings.Sinusoid(
-                timestamps * 3, out_dimension, dtype=torch.complex64
+                timestamps * 3, out_dimension, dtype=torch.float64
+            )
+            self.kernel3 = embeddings.Sinusoid(
+                timestamps * 3, out_dimension, dtype=torch.float64
             )
 
     # Calculate magnitudes of signals
@@ -64,28 +70,40 @@ class HdcRbfEncoder(torch.nn.Module):
                 input=input[:, 3], pad=(0, padding), mode="constant", value=0
             )
             x_fft_signal = F.pad(
-                input=fft_signals[:, 0], pad=(0, padding), mode="constant", value=0
+                input=fft_signals[:, 0].real, pad=(0, padding), mode="constant", value=0
             )
             y_fft_signal = F.pad(
-                input=fft_signals[:, 1], pad=(0, padding), mode="constant", value=0
+                input=fft_signals[:, 1].real, pad=(0, padding), mode="constant", value=0
             )
             z_fft_signal = F.pad(
-                input=fft_signals[:, 2], pad=(0, padding), mode="constant", value=0
+                input=fft_signals[:, 2].real, pad=(0, padding), mode="constant", value=0
+            )
+            x_fft_i_signal = F.pad(
+                input=fft_signals[:, 0].imag, pad=(0, padding), mode="constant", value=0
+            )
+            y_fft_i_signal = F.pad(
+                input=fft_signals[:, 1].imag, pad=(0, padding), mode="constant", value=0
+            )
+            z_fft_i_signal = F.pad(
+                input=fft_signals[:, 2].imag, pad=(0, padding), mode="constant", value=0
             )
         else:
             x_signal = input[:, 1]
             y_signal = input[:, 2]
             z_signal = input[:, 3]
-            x_fft_signal = fft_signals[:, 0]
-            y_fft_signal = fft_signals[:, 1]
-            z_fft_signal = fft_signals[:, 2]
-        features1 = torch.cat((x_signal, y_signal, z_signal)).to(dtype=torch.complex64)
-        features2 = torch.cat((x_fft_signal, y_fft_signal, z_fft_signal)).to(
-            dtype=torch.complex64
-        )
+            x_fft_signal = fft_signals[:, 0].real
+            y_fft_signal = fft_signals[:, 1].real
+            z_fft_signal = fft_signals[:, 2].real
+            x_fft_i_signal = fft_signals[:, 0].imag
+            y_fft_i_signal = fft_signals[:, 1].imag
+            z_fft_i_signal = fft_signals[:, 2].imag
+        features1 = torch.cat((x_signal, y_signal, z_signal))
+        features2 = torch.cat((x_fft_signal, y_fft_signal, z_fft_signal))
+        features3 = torch.cat((x_fft_i_signal, y_fft_i_signal, z_fft_i_signal))
         # Use kernel encoder
         sample_hv1 = self.kernel1(features1)
         sample_hv2 = self.kernel2(features2)
-        sample_hv = sample_hv1 + sample_hv2
+        sample_hv3 = self.kernel3(features3)
+        sample_hv = sample_hv1 + sample_hv2 * sample_hv3
         sample_hv = torch.tanh(sample_hv)
         return sample_hv.squeeze(0)
