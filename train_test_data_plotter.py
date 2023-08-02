6104544fe2356e325e15044a9ce85341f3899fb5
Manuel Segura <Manuel.E.Segura@jpl.nasa.gov>
2023-08-02 14:45:53 -0700

    Update data plotter (work in progress)

--- train_test_data_plotter.py
+++ train_test_data_plotter.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 import csv
 
 
-def plot_pid_train(pid: str):
-    print("Plotting Train Data for person %s" % (pid))
+def plot_train():
+    print("Plotting Train Data")
     train_data = []
     with open(
-        "data/plot_data/train_data/%s_train_data_downsampled.csv" % pid, "r", newline=""
+        "data/plot_data/train_data/train_data_downsampled.csv", "r", newline=""
     ) as file:
         reader = csv.reader(file)
         next(reader)
@@ -26,24 +26,24 @@ def plot_pid_train(pid: str):
     plot_y_z = [float(v[3]) for v in train_data]
     plot_y_tag = [int(float(v[4])) for v in train_data]
 
-    pid_plot_fig = plt.figure()
+    plot_fig = plt.figure()
     plt.plot(plot_x, plot_y_x, label="X")
     plt.plot(plot_x, plot_y_y, label="Y")
     plt.plot(plot_x, plot_y_z, label="Z")
     plt.plot(plot_x, plot_y_tag, label="Tag")
     plt.legend()
-    plt.title("Train Data for %s" % pid)
+    plt.title("Train Data")
     plt.xlabel("Time")
     plt.ylabel("Values")
-    plt.savefig("data/plot_data/train_data/train_data_%s.png" % pid)
+    plt.savefig("data/plot_data/train_data/train_data.png")
     plt.close()
 
 
-def plot_pid_test(pid: str):
-    print("Plotting Test Data for person %s" % (pid))
+def plot_test():
+    print("Plotting Test Data")
     train_data = []
     with open(
-        "data/plot_data/test_data/%s_test_data_downsampled.csv" % pid, "r", newline=""
+        "data/plot_data/test_data/test_data_downsampled.csv", "r", newline=""
     ) as file:
         reader = csv.reader(file)
         next(reader)
@@ -58,38 +58,21 @@ def plot_pid_test(pid: str):
     plot_y_tag = [int(float(v[4])) for v in train_data]
     plot_y_pred = [int(float(v[5])) for v in train_data]
 
-    pid_plot_fig = plt.figure()
+    plot_fig = plt.figure()
     plt.plot(plot_x, plot_y_x, label="X")
     plt.plot(plot_x, plot_y_y, label="Y")
     plt.plot(plot_x, plot_y_z, label="Z")
     plt.plot(plot_x, plot_y_tag, label="Tag")
     plt.plot(plot_x, plot_y_pred, label="Tag Pred")
     plt.legend()
-    plt.title("Test Data for %s" % pid)
+    plt.title("Test Data")
     plt.xlabel("Time")
     plt.ylabel("Values")
-    plt.savefig("data/plot_data/test_data/test_data_%s.png" % pid)
+    plt.savefig("data/plot_data/test_data/test_data.png")
     plt.close()
 
 
 if __name__ == "__main__":
-    PIDS = [
-        "BK7610",
-        "BU4707",
-        "CC6740",
-        "DC6359",
-        "DK3500",
-        "HV0618",
-        "JB3156",
-        "JR8022",
-        "MC7070",
-        "MJ8002",
-        "PC6771",
-        "SA0297",
-        "SF3079",
-    ]
-
-    for pid in PIDS:
-        plot_pid_train(pid)
-        plot_pid_test(pid)
+    plot_train()
+    plot_test()
     print("Done")
