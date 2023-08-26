fe5deb6564dd81daaa1290e5b0cfd373ac53a02b
Manuel Segura <mesegur1@uci.edu>
2023-08-26 15:39:03 -0700

    Bind set construction

--- feature_correlation.py
+++ feature_correlation.py
@@ -5,21 +5,58 @@ from tqdm import tqdm
 
 PIDS = [
     "BK7610",
-    "BU4707",
-    "CC6740",
-    "DC6359",
-    "DK3500",
-    "HV0618",
-    "JB3156",
-    "JR8022",
-    "MC7070",
-    "MJ8002",
-    "PC6771",
-    "SA0297",
-    "SF3079",
+    # "BU4707",
+    # "CC6740",
+    # "DC6359",
+    # "DK3500",
+    # "HV0618",
+    # "JB3156",
+    # "JR8022",
+    # "MC7070",
+    # "MJ8002",
+    # "PC6771",
+    # "SA0297",
+    # "SF3079",
 ]
 
 df = pd.DataFrame()
+
+class Graph:
+    # init function to declare class variables
+    def __init__(self, V):
+        self.V = V
+        self.adj = [[] for i in range(V)]
+ 
+    def dfs_util(self, temp, v, visited):
+        # Mark the current vertex as visited
+        visited[v] = True
+        # Store the vertex to list
+        temp.append(v)
+        # Repeat for all vertices adjacent
+        # to this vertex v
+        for i in self.adj[v]:
+            if visited[i] == False:
+                # Update the list
+                temp = self.dfs_util(temp, i, visited)
+        return temp
+ 
+    # method to add an undirected edge
+    def add_edge(self, v, w):
+        self.adj[v].append(w)
+        self.adj[w].append(v)
+ 
+    # Method to retrieve connected components
+    # in an undirected graph
+    def connected_components(self):
+        visited = []
+        cc = []
+        for i in range(self.V):
+            visited.append(False)
+        for v in range(self.V):
+            if visited[v] == False:
+                temp = []
+                cc.append(self.dfs_util(temp, v, visited))
+        return cc
 
 def load_pid_data(pid : str):
     global df
@@ -39,46 +76,52 @@ def calculate_avg_corr():
     global df
     #Find correlation
     corr = df.corr(method='pearson')
+    print("Correlation matrix shape: ", corr.shape)
 
     max_tac_corr = np.max([corr.iat[0, f] for f in range(1, len(corr))])
     print("Max correlation with TAC = %.5f" % max_tac_corr)
 
     keep = []
     for f in range(1, len(corr)):
-        if corr.iat[0, f] > max_tac_corr*0.1:
+        if corr.iat[0, f] > 0:
             keep.append(f)
     print("keep: ", keep)
     
-    bind = []
-    bundle = []
-    no_connect = []
-
+    #Form graph where edges are correlation above threshold
+    threshold = 0.5
+    g = Graph(len(corr))
     for f_x in keep:
         for f_y in keep:
             if f_x == f_y:
                 continue
-            if corr.iat[f_x, f_y] > 0.8:
-                if (f_y, f_x) not in bundle:
-                    bundle.append((f_x, f_y))
-            elif corr.iat[f_x, f_y] >= 0.5:
-                if (f_y, f_x) not in bind:
-                    bind.append((f_x, f_y))
-            else:
-                if (f_y, f_x) not in no_connect:
-                    no_connect.append((f_x, f_y))
+            c = corr.iat[f_x, f_y]
+            if c > threshold:
+                g.add_edge(f_x, f_y)
+    #Get lists of binded features
+    bind_sets = g.connected_components()
+    bind_sets = [s for s in bind_sets if s[0] in keep]
 
     print("Outputting bind/bundle schema to file")
     with open("data/feature_bind_bundle_schema.txt", "w") as file:
-        file.write("Bundled feature pairs: ")
-        for t in bundle:
-            file.write("(%d, %d), " % t)
-        file.write("\nBind feature pairs: ")
-        for t in bind:
-            file.write("(%d, %d), " % t)
-        file.write("\nNot connected feature pairs: ")
-        for t in no_connect:
-            file.write("(%d, %d), " % t)
-        file.close()
+        file.write("Kept features: \n")
+        for k in keep:
+            file.write("%d, " % k)        
+        file.write("\nBundled/bind schema (%d sets): \n" % len(bind_sets))
+        for s in bind_sets:
+            st = str(s) + "\n"
+            file.write(st)
+
+    print("Generating code stubs")
+    generate_code_stubs(keep, bind_sets)
+
+def generate_code_stubs(keep : list, bind_sets : list):
+    with open("data/feature_code_stubs.txt", "w") as file:
+        s = "feat_hvs = {{}}\n"
+        s2 = "feat_hvs[%d] = self.feat_kernels[%d](feat[%d].unsqueeze(0))\n"
+        file.write(s)
+        for k in keep:
+            file.write(s2 % (k, k, k-1))
+
 
 if __name__ == "__main__":
     for pid in PIDS:
