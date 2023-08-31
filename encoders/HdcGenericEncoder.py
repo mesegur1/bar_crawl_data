e72fa6f6c9eb7bb4595dd1d7459fe47dffc98aa9
Manuel Segura <Manuel.E.Segura@jpl.nasa.gov>
2023-08-30 17:21:43 -0700

    Save work

--- encoders/HdcGenericEncoder.py
+++ encoders/HdcGenericEncoder.py
@@ -7,30 +7,8 @@ from torchhd_custom import functional
 
 NUM_CHANNEL = 4
 NGRAM_SIZE = 3
-
-NUM_FEAT_ITEMS_MFCC = 6
-NUM_FEAT_ITEMS_GENERAL = 3
-NUM_FEAT_GENERAL = 18
-
-MFCC_START = 0
-RMS_START = MFCC_START + NUM_FEAT_ITEMS_MFCC
-MEAN_START = RMS_START + NUM_FEAT_ITEMS_GENERAL
-MEDIAN_START = MEAN_START + NUM_FEAT_ITEMS_GENERAL
-STD_START = MEDIAN_START + NUM_FEAT_ITEMS_GENERAL
-ABS_MAX_START = STD_START + NUM_FEAT_ITEMS_GENERAL
-ABS_MIN_START = ABS_MAX_START + NUM_FEAT_ITEMS_GENERAL
-FFT_MAX_START = ABS_MIN_START + NUM_FEAT_ITEMS_GENERAL
-ZERO_CROSS_RATE_START = FFT_MAX_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_ENTROPY_START = ZERO_CROSS_RATE_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_ENTROPY_FFT_START = SPECTRAL_ENTROPY_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_CENTROID_START = SPECTRAL_ENTROPY_FFT_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_SPREAD_START = SPECTRAL_CENTROID_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_FLUX_START = SPECTRAL_SPREAD_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_ROLLOFF_START = SPECTRAL_FLUX_START + NUM_FEAT_ITEMS_GENERAL
-SPECTRAL_PEAK_RATIO_START = SPECTRAL_ROLLOFF_START + NUM_FEAT_ITEMS_GENERAL
-SKEWNESS_START = SPECTRAL_PEAK_RATIO_START + NUM_FEAT_ITEMS_GENERAL
-KURTOSIS_START = SKEWNESS_START + NUM_FEAT_ITEMS_GENERAL
-AVG_POWER_START = KURTOSIS_START + NUM_FEAT_ITEMS_GENERAL
+MFCC_COV_FEAT_LENGTH = 91
+MFCC_COV_NUM = 6
 
 
 # HDC Encoder for Bar Crawl Data
@@ -43,25 +21,13 @@ class HdcGenericEncoder(torch.nn.Module):
         self.embed = embeddings.Level(levels, out_dimension, dtype=torch.float64)
 
         #Embeddings for extracted feature data
-        self.feat_mfcc_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_MFCC, out_dimension, dtype=torch.float64)
-        self.feat_rms_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_mean_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_median_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_std_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_abs_max_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_abs_min_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_fft_max_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_zero_cross_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_entropy_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_entropy_fft_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_centroid_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_spread_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_flux_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_rolloff_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_spectral_peak_ratio_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_skewness_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_kurtosis_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
-        self.feat_avg_power_kernel = embeddings.Sinusoid(NUM_FEAT_ITEMS_GENERAL, out_dimension, dtype=torch.float64)
+        chosen_feat = [547, 550, 551, 553, 554, 556, 559, 562, 565, 570, 576, 579, 583, 584, 585, 588, 593, 594, 595, 598, ]
+        self.feat_kernels = {}
+        for f in chosen_feat:
+            self.feat_kernels[f] = embeddings.Sinusoid(1, out_dimension, dtype=torch.float64, device="cuda")
+        self.mfcc_feat_kernels = []
+        for _ in range(6):
+            self.mfcc_feat_kernels.append(embeddings.Sinusoid(MFCC_COV_FEAT_LENGTH, out_dimension, dtype=torch.float64, device="cuda"))
 
     # Encode window of raw features (t,x,y,z) and extracted feature vectors (f,)
     def forward(self, signals: torch.Tensor, feat: torch.Tensor) -> torch.Tensor:
@@ -71,34 +37,53 @@ class HdcGenericEncoder(torch.nn.Module):
         )
         sample_hv = torchhd.multiset(sample_hvs)
         # Encode calculated features
-        feat_mffc_hv = self.feat_mfcc_kernel(feat[MFCC_START:MFCC_START+NUM_FEAT_ITEMS_MFCC])
-        feat_rms_hv = self.feat_rms_kernel(feat[RMS_START:RMS_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_mean_hv = self.feat_mean_kernel(feat[MEAN_START:MEAN_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_median_hv = self.feat_median_kernel(feat[MEDIAN_START:MEDIAN_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_std_hv = self.feat_std_kernel(feat[STD_START:STD_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_abs_max_hv = self.feat_abs_max_kernel(feat[ABS_MAX_START:ABS_MAX_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_abs_min_hv = self.feat_abs_min_kernel(feat[ABS_MIN_START:ABS_MIN_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_fft_max_hv = self.feat_fft_max_kernel(feat[FFT_MAX_START:FFT_MAX_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_zero_cross_hv = self.feat_zero_cross_kernel(feat[ZERO_CROSS_RATE_START:ZERO_CROSS_RATE_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_ent_hv = self.feat_spectral_entropy_kernel(feat[SPECTRAL_ENTROPY_START:SPECTRAL_ENTROPY_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_ent_fft_hv = self.feat_spectral_entropy_fft_kernel(feat[SPECTRAL_ENTROPY_FFT_START:SPECTRAL_ENTROPY_FFT_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_cntd_hv = self.feat_spectral_centroid_kernel(feat[SPECTRAL_CENTROID_START:SPECTRAL_CENTROID_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_sprd_hv = self.feat_spectral_spread_kernel(feat[SPECTRAL_SPREAD_START:SPECTRAL_SPREAD_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_flux_hv = self.feat_spectral_flux_kernel(feat[SPECTRAL_FLUX_START:SPECTRAL_FLUX_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_rolloff_hv = self.feat_spectral_rolloff_kernel(feat[SPECTRAL_ROLLOFF_START:SPECTRAL_ROLLOFF_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_spctl_peak_r_hv = self.feat_spectral_peak_ratio_kernel(feat[SPECTRAL_PEAK_RATIO_START:SPECTRAL_PEAK_RATIO_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_skew_hv = self.feat_skewness_kernel(feat[SKEWNESS_START:SKEWNESS_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_kurt_hv = self.feat_kurtosis_kernel(feat[KURTOSIS_START:KURTOSIS_START+NUM_FEAT_ITEMS_GENERAL])
-        feat_avg_pwr_hv = self.feat_avg_power_kernel(feat[AVG_POWER_START:AVG_POWER_START+NUM_FEAT_ITEMS_GENERAL])
+        feat_hvs = {}
+        feat_hvs[547] = self.feat_kernels[547](feat[546].unsqueeze(0))
+        feat_hvs[550] = self.feat_kernels[550](feat[549].unsqueeze(0))
+        feat_hvs[551] = self.feat_kernels[551](feat[550].unsqueeze(0))
+        feat_hvs[553] = self.feat_kernels[553](feat[552].unsqueeze(0))
+        feat_hvs[554] = self.feat_kernels[554](feat[553].unsqueeze(0))
+        feat_hvs[556] = self.feat_kernels[556](feat[555].unsqueeze(0))
+        feat_hvs[559] = self.feat_kernels[559](feat[558].unsqueeze(0))
+        feat_hvs[562] = self.feat_kernels[562](feat[561].unsqueeze(0))
+        feat_hvs[565] = self.feat_kernels[565](feat[564].unsqueeze(0))
+        feat_hvs[570] = self.feat_kernels[570](feat[569].unsqueeze(0))
+        feat_hvs[576] = self.feat_kernels[576](feat[575].unsqueeze(0))
+        feat_hvs[579] = self.feat_kernels[579](feat[578].unsqueeze(0))
+        feat_hvs[583] = self.feat_kernels[583](feat[582].unsqueeze(0))
+        feat_hvs[584] = self.feat_kernels[584](feat[583].unsqueeze(0))
+        feat_hvs[585] = self.feat_kernels[585](feat[584].unsqueeze(0))
+        feat_hvs[588] = self.feat_kernels[588](feat[587].unsqueeze(0))
+        feat_hvs[593] = self.feat_kernels[593](feat[592].unsqueeze(0))
+        feat_hvs[594] = self.feat_kernels[594](feat[593].unsqueeze(0))
+        feat_hvs[595] = self.feat_kernels[595](feat[594].unsqueeze(0))
+        feat_hvs[598] = self.feat_kernels[598](feat[597].unsqueeze(0))
+        mfcc_feat_hvs = []
+        for i in range(MFCC_COV_NUM):
+            mfcc_feat_hvs.append(self.mfcc_feat_kernels[i](feat[i*MFCC_COV_FEAT_LENGTH:(i+1)*MFCC_COV_FEAT_LENGTH]))
 
+        #Combine hypervectors
+        mfcc_hv = torchhd.multibind(torch.concat(mfcc_feat_hvs, dim=0))
         sample_hv = (
             sample_hv
-            * (feat_rms_hv + feat_zero_cross_hv + feat_skew_hv + feat_kurt_hv + feat_avg_pwr_hv)
-            * (feat_mean_hv + feat_median_hv + feat_std_hv + feat_abs_max_hv + feat_abs_min_hv)
-            * (feat_fft_max_hv)
-            * (feat_mffc_hv + feat_spctl_ent_hv + feat_spctl_ent_fft_hv + feat_spctl_cntd_hv + 
-               feat_spctl_sprd_hv + feat_spctl_flux_hv + feat_spctl_rolloff_hv + feat_spctl_peak_r_hv)
+            * (
+                + (feat_hvs[547] * feat_hvs[565] * feat_hvs[562])
+                + (feat_hvs[550] * feat_hvs[553])
+                + (feat_hvs[551] * feat_hvs[554])
+                + (feat_hvs[556])
+                + (feat_hvs[559])
+                + (feat_hvs[570] * feat_hvs[588] * feat_hvs[576] * feat_hvs[579])
+                + (feat_hvs[583])
+                + (feat_hvs[584])
+                + (feat_hvs[585])
+                + (feat_hvs[593])
+                + (feat_hvs[594])
+                + (feat_hvs[595])
+                + (feat_hvs[598])
+                + mfcc_hv
+            )
         )
+        #sample_hv = sample_hv * torchhd.multibind(torch.concat(mfcc_feat_hvs, dim=0))
 
         # Apply activation function
         sample_hv = torchhd.hard_quantize(sample_hv)
