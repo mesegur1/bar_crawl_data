438385385aeaaa7b230700a1879292adaa53b3df
Manuel Segura <mesegur1@uci.edu>
2023-10-18 13:42:06 -0700

    Add false neg/pos metrics

--- metrics.py
+++ metrics.py
@@ -5,6 +5,7 @@ from sklearn.metrics import recall_score
 from sklearn.metrics import precision_score
 from sklearn.metrics import balanced_accuracy_score
 from sklearn.metrics import accuracy_score
+from sklearn.metrics import confusion_matrix
 import getopt, sys
 
 if __name__ == "__main__":
@@ -36,9 +37,17 @@ if __name__ == "__main__":
             for i in range(len(y_true)):
                 preds.append(np.array(next(reader)))
             preds = np.array(preds)
+
+            c_matrix = confusion_matrix(y_true, y_preds)
+            t_pos = c_matrix[1][1]
+            t_neg = c_matrix[0][0]
+            f_pos = c_matrix[0][1]
+            f_neg = c_matrix[1][0]
+
             precision = precision_score(y_true, y_preds)
             recall = recall_score(y_true, y_preds)
             b_acc = balanced_accuracy_score(y_true, y_preds)
+
             sober_data = [d for d in list(zip(y_true, y_preds)) if d[0] == 0]
             sober_true = np.array([d[0] for d in sober_data])
             sober_pred = np.array([d[1] for d in sober_data])
@@ -49,11 +58,16 @@ if __name__ == "__main__":
             drunk_pred = np.array([d[1] for d in drunk_data])
             drunk_acc = accuracy_score(drunk_true, drunk_pred)
 
+
             print("Precision = %f" % precision)
             print("Recall = %f" % recall)
             print("Balanced Accuracy = %f" % b_acc)
             print("Sober Accuracy = %f" % sober_acc)
             print("Drunk Accuracy = %f" % drunk_acc)
+            print("False negatives = %d" % f_neg)
+            print("False postives = %d" % f_pos)
+            print("True negatives = %d" % t_neg)
+            print("True positives = %d" % t_pos)
     except Exception as err:
         print(str(err))
 
