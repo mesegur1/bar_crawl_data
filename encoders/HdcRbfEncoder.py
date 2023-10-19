0aae5aaf893cd00dae4e61ffdb325f487f438162
Manuel Segura <mesegur1@uci.edu>
2023-10-19 16:58:28 -0700

    Add alternate encoder embedding tests

--- encoders/HdcRbfEncoder.py
+++ encoders/HdcRbfEncoder.py
@@ -27,10 +27,7 @@ class HdcRbfEncoder(torch.nn.Module):
         self.device = device
 
         #Embeddings for extracted feature data
-        #self.feat_emb = embeddings.DensityFlocet(18, out_dimension, dtype=torch.float64)
-        self.feat_emb = {}
-        for i in range(18):
-            self.feat_emb[i] = embeddings.Sinusoid(1, out_dimension, dtype=torch.float64, device=self.device)
+        self.feat_emb = embeddings.DensityFlocet(120, out_dimension, dtype=torch.float64)
 
     # Encode window of feature vectors (x,y,z) and feature vectors (f,)
     def forward(self, input: torch.Tensor, feat: torch.Tensor) -> torch.Tensor:
@@ -56,39 +53,19 @@ class HdcRbfEncoder(torch.nn.Module):
         # Use kernel encoder
         sample_hv = self.kernel(accel_features)
 
-        feat_hvs = {}
-        feat_hvs[558] = self.feat_emb[0](feat[558].unsqueeze(0))
-        feat_hvs[582] = self.feat_emb[1](feat[582].unsqueeze(0))
-        feat_hvs[554] = self.feat_emb[2](feat[554].unsqueeze(0))
-        feat_hvs[552] = self.feat_emb[3](feat[552].unsqueeze(0))
-        feat_hvs[93] = self.feat_emb[4](feat[93].unsqueeze(0))
-        feat_hvs[555] = self.feat_emb[5](feat[555].unsqueeze(0))
-        feat_hvs[580] = self.feat_emb[6](feat[580].unsqueeze(0))
-        feat_hvs[571] = self.feat_emb[7](feat[571].unsqueeze(0))
-        feat_hvs[574] = self.feat_emb[8](feat[574].unsqueeze(0))
-        feat_hvs[578] = self.feat_emb[9](feat[578].unsqueeze(0))
-        feat_hvs[566] = self.feat_emb[10](feat[566].unsqueeze(0))
-        feat_hvs[287] = self.feat_emb[11](feat[287].unsqueeze(0))
-        feat_hvs[556] = self.feat_emb[12](feat[556].unsqueeze(0))
-        feat_hvs[550] = self.feat_emb[13](feat[550].unsqueeze(0))
-        feat_hvs[14] = self.feat_emb[14](feat[14].unsqueeze(0))
-        feat_hvs[551] = self.feat_emb[15](feat[551].unsqueeze(0))
-        feat_hvs[64] = self.feat_emb[16](feat[64].unsqueeze(0))
-        feat_hvs[581] = self.feat_emb[17](feat[581].unsqueeze(0))
+        # 20% of features (most important)
+        feat_hv = self.feat_emb(feat[[557, 581, 553, 551, 92, 554, 579, 570, 573, 577, 565, 286, 
+                                      555, 549, 13, 550, 63, 580, 556, 564, 0, 576, 567, 552, 578, 
+                                      588, 597, 566, 571, 44, 572, 574, 14, 582, 381, 594, 4, 593, 
+                                      218, 25, 84, 592, 3, 591, 547, 561, 562, 548, 319, 596, 558, 
+                                      563, 87, 65, 599, 17, 88, 2, 49, 309, 6, 81, 15, 590, 589, 
+                                      43, 273, 420, 546, 568, 400, 277, 202, 287, 434, 435, 423, 
+                                      431, 301, 417, 412, 205, 179, 327, 176, 442, 172, 450, 391, 
+                                      163, 154, 480, 485, 490, 491, 498, 503, 507, 509, 452, 239, 
+                                      388, 219, 303, 292, 310, 316, 320, 322, 324, 326, 330, 336, 
+                                      263, 262, 339, 340, 256, 345, 347]])
 
-        feat_hv = ((feat_hvs[14] + feat_hvs[287])
-                    * (feat_hvs[64])
-                    * (feat_hvs[93] + feat_hvs[574] + feat_hvs[580] + feat_hvs[582] + feat_hvs[555] + feat_hvs[556] + feat_hvs[581])
-                    * (feat_hvs[550])
-                    * (feat_hvs[551] + feat_hvs[554])
-                    * (feat_hvs[552])
-                    * (feat_hvs[558])
-                    * (feat_hvs[566])
-                    * (feat_hvs[571])
-                    * (feat_hvs[578])
-        )
-
-        combined_hv = sample_hv + feat_hv
+        combined_hv = sample_hv + feat_hv + sample_hv * feat_hv
 
         # Apply activation function
         combined_hv = torchhd.hard_quantize(combined_hv)
--- encoders/HdcSinusoidNgramEncoder.py
+++ encoders/HdcSinusoidNgramEncoder.py
@@ -5,28 +5,7 @@ import torchhd
 from torchhd_custom import embeddings
 
 NUM_CHANNEL = 3
-NGRAM_SIZE = 3
-
-NUM_RMS = 3
-NUM_MFCC = 6
-NUM_FFT_MEAN = 3
-NUM_FFT_MAX = 3
-NUM_FFT_VAR = 3
-NUM_MEAN = 3
-NUM_MAX = 3
-NUM_VAR = 3
-NUM_SPECTRAL_CENTROID = 3
-
-RMS_START = 0
-MFCC_START = RMS_START + NUM_RMS
-FFT_MEAN_START = MFCC_START + NUM_MFCC
-FFT_MAX_START = FFT_MEAN_START + NUM_FFT_MEAN
-FFT_VAR_START = FFT_MAX_START + NUM_FFT_MAX
-MEAN_START = FFT_VAR_START + NUM_FFT_VAR
-MAX_START = MEAN_START + NUM_MEAN
-VAR_START = MAX_START + NUM_MAX
-SP_CNTD_START = VAR_START + NUM_VAR
-
+NGRAM_SIZE = 6
 
 # HDC Encoder for Bar Crawl Data
 class HdcSinusoidNgramEncoder(torch.nn.Module):
@@ -39,10 +18,7 @@ class HdcSinusoidNgramEncoder(torch.nn.Module):
         self.device = device
 
         #Embeddings for extracted feature data
-        #self.feat_emb = embeddings.DensityFlocet(18, out_dimension, dtype=torch.float64)
-        self.feat_emb = {}
-        for i in range(18):
-            self.feat_emb[i] = embeddings.Sinusoid(1, out_dimension, dtype=torch.float64, device=self.device)
+        self.feat_emb = embeddings.DensityFlocet(120, out_dimension, dtype=torch.float64)
 
     # Encode window of feature vectors (x,y,z) and feature vectors (f,)
     def forward(self, input: torch.Tensor, feat: torch.Tensor) -> torch.Tensor:
@@ -53,39 +29,19 @@ class HdcSinusoidNgramEncoder(torch.nn.Module):
         # Perform ngram statistics
         sample_hv = torchhd.ngrams(sample_hv, NGRAM_SIZE)
 
-        feat_hvs = {}
-        feat_hvs[558] = self.feat_emb[0](feat[558].unsqueeze(0))
-        feat_hvs[582] = self.feat_emb[1](feat[582].unsqueeze(0))
-        feat_hvs[554] = self.feat_emb[2](feat[554].unsqueeze(0))
-        feat_hvs[552] = self.feat_emb[3](feat[552].unsqueeze(0))
-        feat_hvs[93] = self.feat_emb[4](feat[93].unsqueeze(0))
-        feat_hvs[555] = self.feat_emb[5](feat[555].unsqueeze(0))
-        feat_hvs[580] = self.feat_emb[6](feat[580].unsqueeze(0))
-        feat_hvs[571] = self.feat_emb[7](feat[571].unsqueeze(0))
-        feat_hvs[574] = self.feat_emb[8](feat[574].unsqueeze(0))
-        feat_hvs[578] = self.feat_emb[9](feat[578].unsqueeze(0))
-        feat_hvs[566] = self.feat_emb[10](feat[566].unsqueeze(0))
-        feat_hvs[287] = self.feat_emb[11](feat[287].unsqueeze(0))
-        feat_hvs[556] = self.feat_emb[12](feat[556].unsqueeze(0))
-        feat_hvs[550] = self.feat_emb[13](feat[550].unsqueeze(0))
-        feat_hvs[14] = self.feat_emb[14](feat[14].unsqueeze(0))
-        feat_hvs[551] = self.feat_emb[15](feat[551].unsqueeze(0))
-        feat_hvs[64] = self.feat_emb[16](feat[64].unsqueeze(0))
-        feat_hvs[581] = self.feat_emb[17](feat[581].unsqueeze(0))
+        # 20% of features (most important)
+        feat_hv = self.feat_emb(feat[[557, 581, 553, 551, 92, 554, 579, 570, 573, 577, 565, 286, 
+                                      555, 549, 13, 550, 63, 580, 556, 564, 0, 576, 567, 552, 578, 
+                                      588, 597, 566, 571, 44, 572, 574, 14, 582, 381, 594, 4, 593, 
+                                      218, 25, 84, 592, 3, 591, 547, 561, 562, 548, 319, 596, 558, 
+                                      563, 87, 65, 599, 17, 88, 2, 49, 309, 6, 81, 15, 590, 589, 
+                                      43, 273, 420, 546, 568, 400, 277, 202, 287, 434, 435, 423, 
+                                      431, 301, 417, 412, 205, 179, 327, 176, 442, 172, 450, 391, 
+                                      163, 154, 480, 485, 490, 491, 498, 503, 507, 509, 452, 239, 
+                                      388, 219, 303, 292, 310, 316, 320, 322, 324, 326, 330, 336, 
+                                      263, 262, 339, 340, 256, 345, 347]])
 
-        feat_hv = ((feat_hvs[14] + feat_hvs[287])
-                    * (feat_hvs[64])
-                    * (feat_hvs[93] + feat_hvs[574] + feat_hvs[580] + feat_hvs[582] + feat_hvs[555] + feat_hvs[556] + feat_hvs[581])
-                    * (feat_hvs[550])
-                    * (feat_hvs[551] + feat_hvs[554])
-                    * (feat_hvs[552])
-                    * (feat_hvs[558])
-                    * (feat_hvs[566])
-                    * (feat_hvs[571])
-                    * (feat_hvs[578])
-        )
-
-        combined_hv = sample_hv + feat_hv
+        combined_hv = sample_hv + feat_hv + sample_hv * feat_hv
 
         # Apply activation function
         combined_hv = torchhd.hard_quantize(combined_hv)
